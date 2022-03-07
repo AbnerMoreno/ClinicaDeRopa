d96248f02dd0118a928bca4414d3fefd780a4c57
AbnerMoreno <abner2120@hotmail.com>
2022-03-06 23:40:15 -0500

    the css and html of the aboutus section are started

--- src/app/aboutus/aboutus.module.ts
+++ src/app/aboutus/aboutus.module.ts
@@ -1,3 +1,4 @@
+import { ComponentsModule } from './../components/components.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -13,7 +14,8 @@ import { AboutusPage } from './aboutus.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    AboutusPageRoutingModule
+    AboutusPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [AboutusPage]
 })
