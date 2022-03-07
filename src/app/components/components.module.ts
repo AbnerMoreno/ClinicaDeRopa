35e2ca4d1ca27a7ba1afc3b7336dd44671ee6bed
Abnercuellar <abner2120@hotmail.com>
2022-03-07 10:10:34 -0500

    se elimina antiguo header

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
 import { FooterComponent } from './footer/footer.component';
 import { HeadersPage } from './headers/headers.page';
@@ -10,12 +9,10 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    HeaderComponent,
     FooterComponent,
     HeadersPage
   ],
   exports:[
-    HeaderComponent,
     FooterComponent,
     HeadersPage
   ],
